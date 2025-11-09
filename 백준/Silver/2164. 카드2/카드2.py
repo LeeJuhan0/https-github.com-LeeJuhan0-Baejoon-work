e5313f2e212b27d84440731935d5bcfc28f0e81c
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2025-11-09 21:46:17 +0900

    Update 카드2.py

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,10 +1,23 @@
-from collections import deque
-
 n = int(input())
-dq = deque(range(1, n+1))
+card_list = []
+card_list2 = []
+for i in range(n):
+    card_list.append(i+1)
+card_list.reverse()    
 
-while len(dq) > 1:
-    dq.popleft()
-    dq.append(dq.popleft())
+def card2(card_list, card_list2):
+    while len(card_list) + len(card_list2) > 1:
+        if len(card_list) == 0 :
+            card_list, card_list2 = card_list2, card_list
+            card_list.reverse()
+        if len(card_list) != 1:
+            card_list.pop()
+            card_list2.append(card_list.pop())
+        else :
+            card_list.pop()
+            card_list, card_list2 = card_list2, card_list
+            card_list.reverse()
+            card_list2.append(card_list.pop())
+    return card_list[0] if card_list else card_list2[0]
 
-print(dq[0])
+print(card2(card_list,card_list2))

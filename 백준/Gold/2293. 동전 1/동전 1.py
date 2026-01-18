8d3172dbdceb05d590b996ec469f26fdaea3466b
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2026-01-18 19:06:48 +0900

    [Gold IV] Title: 동전 1, Time: 192 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+coin = []
+for i in range(n) :
+    coin.append(int(input()))
+dp =[0 for i in range(k+1)]
+dp[0] = 1
+for i in coin:
+    for j in range(i, k+1):
+        dp[j] += dp[j-i]
+print(dp[-1])

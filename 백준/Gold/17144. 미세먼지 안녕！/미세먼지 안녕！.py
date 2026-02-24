5b8e129ad3b52f1a04f40d4a1839c91f379c0052
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2026-02-24 13:41:35 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 3924 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,40 +1,40 @@
 n, m, t = map(int, input().split())
-map = [list(map(int, input().split())) for _ in range(n)]
-next_map = [[0 for _ in range(m)] for _ in range(n)]
-def flow(map, next_map):
+grid = [list(map(int, input().split())) for _ in range(n)]
+next_grid = [[0 for _ in range(m)] for _ in range(n)]
+def flow(grid, next_grid):
     for x in range(n):
         for y in range(m):
-            if map[x][y] == -1 :
-                next_map[x][y] = -1
+            if grid[x][y] == -1 :
+                next_grid[x][y] = -1
                 continue
-            if map[x][y] < 5:
-                next_map[x][y] += map[x][y]
+            if grid[x][y] < 5:
+                next_grid[x][y] += grid[x][y]
                 continue
             dxs, dys = [-1,1,0,0], [0,0,1,-1]
             cnt = 0
             for dx, dy in zip(dxs, dys):
                 next_x = x + dx
                 next_y = y +dy
-                if can_go(next_x, next_y, map):
+                if can_go(next_x, next_y, grid):
                     cnt += 1
-                    next_map[next_x][next_y] += map[x][y]//5
-            next_map[x][y] += map[x][y] - (map[x][y]//5) * cnt
+                    next_grid[next_x][next_y] += grid[x][y]//5
+            next_grid[x][y] += grid[x][y] - (grid[x][y]//5) * cnt
 
-    return next_map
+    return next_grid
 
 def in_range(x,y):
     if 0 <= x < n and 0 <= y < m :
         return True
     return False
 
-def can_go(x,y,map) :
+def can_go(x,y,grid) :
     if not in_range(x,y) :
         return False
-    if map[x][y] == -1 :
+    if grid[x][y] == -1 :
         return False
     return True
     
-def wind(map, up, down):
+def wind(grid, up, down):
     up_x, up_y = up[0]-1, up[1]
     up_dxs, up_dys = [-1,0,1,0], [0,1,0,-1]
     indicator = 0
@@ -45,10 +45,10 @@ def wind(map, up, down):
             indicator = (indicator + 1)
             next_up_x = up_x + up_dxs[indicator]
             next_up_y = up_y + up_dys[indicator]
-        if map[next_up_x][next_up_y] == -1 :
-            map[up_x][up_y] = 0
+        if grid[next_up_x][next_up_y] == -1 :
+            grid[up_x][up_y] = 0
             break
-        map[up_x][up_y] = map[next_up_x][next_up_y]
+        grid[up_x][up_y] = grid[next_up_x][next_up_y]
         up_x, up_y = next_up_x, next_up_y
 
     down_x, down_y = down[0] + 1, down[1]
@@ -59,28 +59,28 @@ def wind(map, up, down):
         if not in_range(next_down_x, next_down_y) or (indicator == 2 and next_down_x == down[0] - 1):
             indicator = (indicator + 1)
             next_down_x, next_down_y = down_x + down_dxs[indicator], down_y + down_dys[indicator]
-        if map[next_down_x][next_down_y] == -1:
-            map[down_x][down_y] = 0
+        if grid[next_down_x][next_down_y] == -1:
+            grid[down_x][down_y] = 0
             break
-        map[down_x][down_y] = map[next_down_x][next_down_y]
+        grid[down_x][down_y] = grid[next_down_x][next_down_y]
         down_x, down_y = next_down_x, next_down_y
           
-    return map
+    return grid
 
-def clean_map(n, m, t, map, next_map):
+def clean_map(n, m, t, grid, next_grid):
     machine = []
     for x in range(n):
         for y in range(m):
-            if map[x][y] == -1 :
+            if grid[x][y] == -1 :
                 machine.append([x,y])
     summ = 2
     for i in range(t):
-        map = flow(map, next_map)
-        next_map = [[0 for _ in range(m)] for _ in range(n)]
-        map = wind(map, machine[0], machine[1])
-    for i in map :
+        grid = flow(grid, next_grid)
+        next_grid = [[0 for _ in range(m)] for _ in range(n)]
+        grid = wind(grid, machine[0], machine[1])
+    for i in grid :
         summ += sum(i)
     return summ
 
 
-print(clean_map(n,m,t,map, next_map))
+print(clean_map(n,m,t,grid, next_grid))

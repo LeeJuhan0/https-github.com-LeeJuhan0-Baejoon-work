540373b97becc3e9105e29cb873d9692676b9336
LeeJuhan0 <147235039+LeeJuhan0@users.noreply.github.com>
2026-02-26 21:34:08 +0900

    [level 2] Title: [1차] 캐시, Time: 75.55 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    if cacheSize == 0:
+        return len(cities) * 5
+        
+    answer = 0
+    cache = deque(maxlen=cacheSize) 
+    
+    for city in cities:
+        city = city.lower() 
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+            cache.append(city) 
+            
+        else:
+            answer += 5
+            cache.append(city) 
+            
+    return answer
